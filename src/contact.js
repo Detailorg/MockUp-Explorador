a9abcf225a53c85f1712cbef0b80e49d9c3b8316
VG <victor.gil.cif@gmail.com>
2020-06-04 12:10:57 +0200

    No-cors

--- src/contact.js
+++ src/contact.js
@@ -30,6 +30,7 @@ $(function(){
 
       fetch(urlGETkey, {
         method: 'GET',
+        mode: 'no-cors',
         headers:{
           'Content-Type': 'application/json',
           "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYjZmZDc2YjI1YWVlOWUxMTFmMmY0NyIsIm5hbWUiOiJWIEciLCJpYXQiOjE1ODkwNTA4MzEsImV4cCI6MTY0OTA1MDgzMX0.WXyxFXkZCxQFF9ZXU_-P7wMw2WqJ_xKVq5WSH7zoGJw`
@@ -69,6 +70,7 @@ $(function(){
 
         fetch(urlPOSTfile, {
           method: 'POST',
+          mode: 'no-cors',
           body: data,
           headers:{
             "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYjZmZDc2YjI1YWVlOWUxMTFmMmY0NyIsIm5hbWUiOiJWIEciLCJpYXQiOjE1ODkwNTA4MzEsImV4cCI6MTY0OTA1MDgzMX0.WXyxFXkZCxQFF9ZXU_-P7wMw2WqJ_xKVq5WSH7zoGJw`
@@ -110,6 +112,7 @@ $(function(){
 
           fetch(urlPOST, {
             method: 'POST',
+            mode: 'no-cors',
             body: data,
             headers:{
               'Content-Type': 'application/json',
